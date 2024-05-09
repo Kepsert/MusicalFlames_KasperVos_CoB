c25ce3368b9df3fee8527a528b0d84df2f61f575
Kasper <voskasper@gmail.com>
2024-05-09 20:23:16 +0200

    Slider Animations
    - Slider now slowly glides onto the screen

--- Assets/Scripts/UI/RoundTimerUI.cs
+++ Assets/Scripts/UI/RoundTimerUI.cs
@@ -1,5 +1,7 @@
+using DG.Tweening;
 using Messaging;
 using Messaging.Messages;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,10 +9,15 @@ public class RoundTimerUI : MonoBehaviour, IUpdateable<float>
 {
     [SerializeField] Slider _slider = null;
 
+    const float _animationDuration = .75f;
+    const float _moveDistance = 150;
+
     void Start()
     {
         MessageHub.Subscribe<NewGameMessage>(this, NewGameStarted);
         MessageHub.Subscribe<EndGameMessage>(this, GameEnded);
+
+        _slider.transform.position += new Vector3(0, _moveDistance);
     }
 
     private void OnDestroy()
@@ -23,12 +30,26 @@ public class RoundTimerUI : MonoBehaviour, IUpdateable<float>
     {
         _slider.gameObject.SetActive(true);
 
+        StartCoroutine(AnimateToggleCoroutine(true));
+
         MessageHub.Publish(new InjectUIMessage(this));
     }
 
     void GameEnded(EndGameMessage obj)
     {
-        _slider.gameObject.SetActive(false);
+        StartCoroutine(AnimateToggleCoroutine(false));
+    }
+
+    IEnumerator AnimateToggleCoroutine(bool activate)
+    {
+        Vector3 moveDistance = new Vector2(0, _moveDistance);
+        Vector3 endPosition = activate ? _slider.gameObject.transform.position - moveDistance : _slider.transform.position + moveDistance;
+
+        _slider.transform.DOMove(endPosition, _animationDuration * 0.95f).SetEase(Ease.InSine);
+        yield return new WaitForSeconds(_animationDuration);
+
+        if (!activate)
+            _slider.gameObject.SetActive(false);
     }
 
     public void UpdateValue(float value)
