a4c2d3db484a4656a02781cb2d25e8848546142c
Kasper <voskasper@gmail.com>
2024-05-09 20:31:35 +0200

    Animated MainMenuPanel

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -1,15 +1,33 @@
+using DG.Tweening;
 using Messaging;
 using Messaging.Messages;
+using System.Collections;
 using UnityEngine;
 
 public class MainMenuController : MonoBehaviour
 {
     [SerializeField] GameObject _mainMenuPanel = null;
     [SerializeField] SequenceGameSettings[] _gameSettings = null;
+
+    const float _animationDuration = 1.25f;
     
     public void StartGame(int buttonIndex)
     {
+        StartCoroutine(AnimatePanelCoroutine(buttonIndex));
+    }
+
+    IEnumerator AnimatePanelCoroutine(int index)
+    {
+        Vector3 endPosition = _mainMenuPanel.transform.position + new Vector3(0, -1500, 0);
+        _mainMenuPanel.transform.DOMove(endPosition, _animationDuration * 0.95f).SetEase(Ease.OutSine);
+        yield return new WaitForSeconds(_animationDuration);
+
         _mainMenuPanel.SetActive(false);
+        LaunchGame(index);
+    } 
+
+    void LaunchGame(int buttonIndex)
+    {
         SequenceGameSettings gameSettings = _gameSettings[buttonIndex];
 
         if (buttonIndex == 3)
