7ba3795d20afa9b643b2d4fb87bce92ffa3fdc22
Kasper <voskasper@gmail.com>
2024-05-09 13:34:26 +0200

    InputController
    - Created an inputcontroller script
    - Gives value 1 through 5 depending on what button is pressed

--- Assets/Scripts/Input/InputController.cs
+++ Assets/Scripts/Input/InputController.cs
@@ -1,18 +1,58 @@
+using Messaging;
+using Messaging.Messages;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    PlayerInput _playerInput;
+    InputAction _toggleCandleAction;
+
+    bool _hasControl = false;
+
+    void Awake()
     {
-        
+        _playerInput = GetComponent<PlayerInput>();
+        if (_playerInput != null)
+        {
+            _toggleCandleAction = _playerInput.currentActionMap.FindAction("ToggleCandle");
+        }
+        else
+        {
+            Debug.LogWarning("PlayerInput component missing on " + this.name);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        MessageHub.Subscribe<GameStateChangedMessage>(this, GameStateChanged);
+
+        _toggleCandleAction.performed += ToggleCandleButtonPressed;
+    }
+
+    void OnDestroy()
+    {
+        MessageHub.Unsubscribe<GameStateChangedMessage>(this);
+
+        _toggleCandleAction.performed -= ToggleCandleButtonPressed;
+    }
+
+    private void GameStateChanged(GameStateChangedMessage obj)
+    {
+        if (obj.GameState == GameState.Play)
+            _hasControl = true;
+        else
+            _hasControl = false;
+    }
+
+    private void ToggleCandleButtonPressed(InputAction.CallbackContext obj)
+    {
+        if (_hasControl)
+        {
+            int value = (int)obj.ReadValue<float>();
+        }
     }
 }
